487263fe34896efb23a705296afbb23bfe286116
m-novelli <mah.novelli@hotmail.com>
2025-05-21 10:03:28 -0300

    Update aplica_modelo

--- aplica_modelo.py
+++ aplica_modelo.py
@@ -1,19 +1,23 @@
+
+
 from datas.salva_base_localmente import buscar_dados_historicos
 from models.ML_otimizacao_portfolio import PortfolioOptimizer
 from datetime import datetime
 
 tickers = ['AAPL','GOOG','AMZN', 'NFLX', 'MSFT', 'IBM','^GSPC']
+tickers2 = ['AAPL','GOOG','AMZN', 'NFLX', 'MSFT', 'IBM']
+
 start = "2007-01-01"
 end = datetime.now().strftime("%Y-%m-%d")
 
 df = buscar_dados_historicos(tickers, start, end, intervalo='1d')
 
-optimizer = PortfolioOptimizer(df, tickers, '^GSPC')
+optimizer = PortfolioOptimizer(df, tickers2, '^GSPC')
 
 optimizer.load_data()
 
-optimizer.prepare_ml_features()
+features_df, targets_dict = optimizer.prepare_ml_features()
 
 models_dict = optimizer.read_joblib()
 
-optimizer.optimize_ml_portfolio(models_dict, optimizer.features_df)
+optimizer.optimize_ml_portfolio(models_dict, features_df)
